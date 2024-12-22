25f941d5bce2ff8f1549b2ce9516be99c60ff764
KKV9 <KKV9@noreply.github.com>
2024-12-22 23:24:13 +0000

    Update for yazi 0.4+

--- init.lua
+++ init.lua
@@ -1,16 +1,16 @@
 local M = {}
 
-function M:peek()
-	local file_url = self.file.url
-	if self.file.link_to then
-		file_url = self.file.link_to
+function M:peek(job)
+	local file_url = job.file.url
+	if job.file.link_to then
+		file_url = job.file.link_to
 	end
 
 	local child = Command("bat")
 		:args({
 			"-ppnf",
 			"--terminal-width",
-			tostring(self.area.w),
+			tostring(job.area.w),
 			tostring(file_url),
 		})
 		:stdout(Command.PIPED)
@@ -18,52 +18,52 @@ function M:peek()
 		:spawn()
 
 	if not child then
-		return self:fallback_to_builtin()
+		return job:fallback_to_builtin()
 	end
 
-	local limit = self.area.h
+	local limit = job.area.h
 	local i, lines = 0, ""
 	repeat
 		local next, event = child:read_line()
 		if event == 1 then
-			return self:fallback_to_builtin()
+			return job:fallback_to_builtin()
 		elseif event ~= 0 then
 			break
 		end
 
 		i = i + 1
-		if i > self.skip then
+		if i > job.skip then
 			lines = lines .. next
 		end
-	until i >= self.skip + limit
+	until i >= job.skip + limit
 
 	child:start_kill()
-	if self.skip > 0 and i < self.skip + limit then
+	if job.skip > 0 and i < job.skip + limit then
 		ya.manager_emit(
 			"peek",
-			{ tostring(math.max(0, i - limit)), only_if = tostring(self.file.url), upper_bound = "" }
+			{ tostring(math.max(0, i - limit)), only_if = tostring(job.file.url), upper_bound = "" }
 		)
 	else
 		lines = lines:gsub("\t", string.rep(" ", PREVIEW.tab_size))
-		ya.preview_widgets(self, { ui.Paragraph.parse(self.area, lines) })
+    ya.preview_widgets(job, { ui.Text.parse(lines):area(job.area), })
 	end
 end
 
-function M:seek(units)
+function M:seek(job)
 	local h = cx.active.current.hovered
-	if h and h.url == self.file.url then
-		local step = math.floor(units * self.area.h / 10)
+	if h and h.url == job.file.url then
+		local step = math.floor(units * job.area.h / 10)
 		ya.manager_emit("peek", {
 			tostring(math.max(0, cx.active.preview.skip + step)),
-			only_if = tostring(self.file.url),
+			only_if = tostring(job.file.url),
 		})
 	end
 end
 
 function M:fallback_to_builtin()
-	local _, bound = ya.preview_code(self)
+	local _, bound = ya.preview_code(job)
 	if bound then
-		ya.manager_emit("peek", { tostring(bound), only_if = tostring(self.file.url), upper_bound = "" })
+		ya.manager_emit("peek", { tostring(bound), only_if = tostring(job.file.url), upper_bound = "" })
 	end
 end
 
